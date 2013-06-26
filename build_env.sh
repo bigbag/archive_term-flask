11b85f01f44137e20c79951402a055d100430c97
Lyashkov Pavel <bilbo.kem@gmail.com>
2013-06-26 18:22:16 +0400

    chore: add build files

---
+++ build_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo $0: Creating virtual environment
+virtualenv --prompt="<myenv>" ./env
+
+mkdir ./logs
+mkdir ./pids
+mkdir ./db
+cp gunicorn.conf.py_distr gunicorn.conf.py
+cp api/configs/general.py_distr api/configs/general.py
+
+
+echo $0: Installing dependencies
+source ./env/bin/activate
+export PIP_REQUIRE_VIRTUALENV=true
+./env/bin/pip install --requirement=./requirements.conf --log=./logs/build_pip_packages.log
+
+echo $0: Making virtual environment relocatable
+virtualenv --relocatable ./env
+
+echo $0: Creating virtual environment finished.
