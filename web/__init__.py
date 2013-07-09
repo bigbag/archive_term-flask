2cb1f6facdc6d7db8a2f2dba62953896e11c4130
Mobispot Server <bilbo.kem@gmail.com>
2013-07-09 13:19:24 +0200

    fix: server config

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
-# app.config.from_object('configs.general.DevelopmentConfig')
-app.config.from_object('configs.general.ProductionConfig')
+app.config.from_object('configs.general.DevelopmentConfig')
+#app.config.from_object('configs.general.ProductionConfig')
 
 from flask.ext.cache import Cache
 
