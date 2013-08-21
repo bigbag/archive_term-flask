750f408024bcaab0a0ed4f5986d0dc507ba7f0c3
Lyashkov Pavel <bilbo.kem@gmail.com>
2013-08-21 18:50:48 +0400

    fix: blacklist generator

--- console/view/test.py
+++ console/view/test.py
@@ -36,51 +36,46 @@ class TestCommand(Command):
 
     def run(self):
 
-        with open('tmp/import.csv', 'rb') as csvfile:
-            spamreader = csv.reader(csvfile)
-            for row in spamreader:
-                person = Person.query.filter_by(payment_id=row[1]).first()
+        # with open('tmp/import.csv', 'rb') as csvfile:
+        #     spamreader = csv.reader(csvfile)
+        #     for row in spamreader:
+        #         person = Person.query.filter_by(payment_id=row[1]).first()
 
-                if person:
-                    continue
+        #         if person:
+        #             continue
 
-                person = Person()
-                person.hard_id = row[0]
-                person.payment_id = row[1]
-                person.firm_id = 12
-                person.first_name = row[3]
-                person.last_name = row[2]
+        #         person = Person()
+        #         person.hard_id = row[0]
+        #         person.payment_id = row[1]
+        #         person.firm_id = 12
+        #         person.first_name = row[3]
+        #         person.last_name = row[2]
 
-                if not person.save():
-                    continue
+        #         if not person.save():
+        #             continue
 
-                person_event = PersonEvent()
-                person_event.person_id = person.id
-                person_event.term_id = 39
-                person_event.event_id = 3
-                person_event.firm_id = person.firm_id
-                person_event.timeout = "00:15:00"
-                person_event.save()
+        #         person_event = PersonEvent()
+        #         person_event.person_id = person.id
+        #         person_event.term_id = 39
+        #         person_event.event_id = 3
+        #         person_event.firm_id = person.firm_id
+        #         person_event.timeout = "00:05:00"
+        #         person_event.save()
 
-                person_event = PersonEvent()
-                person_event.person_id = person.id
-                person_event.term_id = 48
-                person_event.event_id = 3
-                person_event.firm_id = person.firm_id
-                person_event.timeout = "00:15:00"
-                person_event.save()
-
-
-
+        #         person_event = PersonEvent()
+        #         person_event.person_id = person.id
+        #         person_event.term_id = 48
+        #         person_event.event_id = 3
+        #         person_event.firm_id = person.firm_id
+        #         person_event.timeout = "00:05:00"
+        #         person_event.save()
 
           # for row in spamreader:
         #     person = Person.query.filter_by(
         #         payment_id=row[1]).first()
-
         #     if person.payment_id == person.hard_id:
         #         person.hard_id = row[0]
         #         print person.save()
-
-        # print self.rate(5, 36)
-        # print self.rate(6, 45)
-        # print self.rate(7, 49)
+        print self.rate(5, 36)
+        print self.rate(6, 45)
+        print self.rate(7, 49)
