fa362b76264b860c14aac09c9d4ee1c91543a958
Lyashkov Pavel <bilbo.kem@gmail.com>
2013-10-11 18:23:27 +0400

    fix: добавил заглушки для отображения страницы сотрудников

---
+++ web/views/term/person.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    Веб интерфейс терминального проекта, люди фасад
+
+    :copyright: (c) 2013 by Pavel Lyashkov.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from web.views.term.general import *
+
+from models.person import Person
+
+
+@mod.route('/person', methods=['GET'])
+@login_required
+def person_view():
+    """Отображаем страницу со списком сотрудников, сам список получаем отдельным запросом"""
+
+    return render_template(
+        'term/person/index.html'
+    )
+
+
+@mod.route('/person', methods=['POST'])
+@login_required
+@json_headers
+def get_person_list():
+    """Получаем список сотрудников фирмы"""
+
+    arg = json.loads(request.stream.read())
+    answer = Person().select_person_list(
+        g.firm_info['id'], **arg)
+
+    return jsonify(answer)
