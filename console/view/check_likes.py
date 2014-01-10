8cc6724c1cc424640570162ecf1ad41d4c215766
Denis Amelin <dionicii@mail.ru>
2014-01-10 16:34:18 +0400

    feature проверка лайков по стеку, и в случае положительного результата - добавление записи в person_event

--- console/view/check_likes.py
+++ console/view/check_likes.py
@@ -5,10 +5,14 @@
     :copyright: (c) 2013 by Denis Amelin.
     :license: BSD, see LICENSE for more details.
 """
+import json
 from flask import Flask
 from flask.ext.script import Command
 from grab import Grab
 from models.loyalty import Loyalty
+from models.payment_wallet import PaymentWallet
+from models.person import Person
+from models.person_event import PersonEvent
 from models.likes_stack import LikesStack
 from models.soc_token import SocToken
 from console.configs.payment import UnitellerConfig
@@ -18,8 +22,6 @@ from libs.socnets_api import SocnetsApi
 class CheckLikes(Command):
 
     def run(self):
-        print "Start CheckSocial:"
-
         lStack = LikesStack.query.filter().all()
 
         for stackItem in lStack:
@@ -35,9 +37,40 @@ class CheckLikes(Command):
                     socToken.type, url, socToken.user_token)
 
                 if pageLiked:
-                    print 'You are liked this page: ' + url
-                    # stackItem.delete()
-                else:
-                    print 'You are not liked this page: ' + url
+                    loyalty = Loyalty.query.filter_by(
+                        id=stackItem.loyalty_id).first()
+                    wallet = PaymentWallet.query.filter_by(
+                        user_id=socToken.user_id).first()
+                    if loyalty and wallet:
+                        person = Person.query.filter_by(
+                            firm_id=loyalty.firm_id, payment_id=wallet.payment_id).first()
+
+                        if not person:
+                            person = Person()
+                            person.name = 'Участник промо-кампании'
+                            person.firm_id = loyalty.firm_id
+                            person.hard_id = wallet.hard_id
+                            person.payment_id = wallet.payment_id
+                            person.save()
+
+                        terms = json.loads(loyalty.terms_id)
+                        for term in terms:
+                            event = PersonEvent.query.filter_by(
+                                person_id=person.id, term_id=term, event_id=loyalty.event_id, firm_id=loyalty.firm_id).first()
+
+                        if event:
+                            if event.timeout > PersonEvent.LIKE_TIMEOUT:
+                                event.status = PersonEvent.STATUS_BANNED
+                                event.save()
+                        else:
+                            event = PersonEvent()
+                            event.person_id = person.id
+                            event.term_id = term
+                            event.event_id = loyalty.event_id
+                            event.firm_id = loyalty.firm_id
+                            event.timeout = PersonEvent.LIKE_TIMEOUT
+                            event.save()
+                    # print 'user is liked this page: ' + url
+                    stackItem.delete()
 
         return True
