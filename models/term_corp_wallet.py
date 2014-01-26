d47723a4503ed8b65c422df33f991807b3fa51fa
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-01-27 00:35:45 +0400

    fix: добавил интервал обнуления корпоративного кошелька

--- models/term_corp_wallet.py
+++ models/term_corp_wallet.py
@@ -19,22 +19,35 @@ class TermCorpWallet(db.Model):
     STATUS_ACTIVE = 1
     STATUS_BANNED = -1
 
+    INTERVAL_ONCE = 0
+    INTERVAL_WEEK = 1
+    INTERVAL_MONTH = 2
+
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     person = db.relationship('Person')
     creation_date = db.Column(db.DateTime, nullable=False)
     balance = db.Column(db.Integer, nullable=False)
     limit = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Integer, nullable=False)
+    interval = db.Column(db.Integer, nullable=False, index=True)
+    status = db.Column(db.Integer, nullable=False, index=True)
 
     def __init__(self):
         self.limit = 0
         self.balance = 0
+        self.interval = self.INTERVAL_ONCE
         self.creation_date = date_helper.get_curent_date()
         self.status = self.STATUS_ACTIVE
 
     def __repr__(self):
         return '<id %r>' % (self.id)
+
+    def get_interval_list(self):
+        return [
+            {'id': self.INTERVAL_ONCE, 'name': u"Не сбрасывать"},
+            {'id': self.INTERVAL_WEEK, 'name': u"Еженедельно"},
+            {'id': self.INTERVAL_MONTH, 'name': u"Ежемесячно"}
+        ]
 
     def delete(self):
         db.session.delete(self)
