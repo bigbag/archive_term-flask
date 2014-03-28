3ebe8c8201ea5cb933d88cb199f28b7bd9a5e9d4
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-03-28 18:40:26 +0400

    fix: united statistics terminals and people

--- web/views/term/report.py
+++ web/views/term/report.py
@@ -13,12 +13,11 @@ from models.report import Report
 @mod.route('/report/<action>', methods=['GET'])
 @login_required
 def report_report_action(action):
-    """Отчеты по сотрудникам, терминалам, оборотам"""
+    """Статистика по корпоративным и личным расходам"""
 
     VALID_ACTITON = (
-        'person',
-        'terminal',
-        'summ',
+        'company',
+        'money',
     )
     if not action in VALID_ACTITON:
         abort(404)
@@ -27,36 +26,29 @@ def report_report_action(action):
     return render_template(template)
 
 
-@mod.route('/report/person', methods=['POST'])
-@login_required
-@json_headers
-def report_get_person_report():
-    arg = get_post_arg(request, True)
-    arg['firm_id'] = g.firm_info['id']
-
-    answer = Report().get_person_report(**arg)
-
-    return jsonify(answer)
-
-
-@mod.route('/report/terminal', methods=['POST'])
+@mod.route('/report/company', methods=['POST'])
 @login_required
 @json_headers
 def report_get_terminal_report():
     arg = get_post_arg(request, True)
-
-    arg['payment_type'] = Report.TYPE_WHITE
     arg['firm_id'] = g.firm_info['id']
 
-    answer = Report().get_term_report(**arg)
+    if not 'report_type' in arg:
+        abort(405)
+
+    if arg['report_type'] == 'term':
+        arg['payment_type'] = Report.TYPE_WHITE
+        answer = Report().get_term_report(**arg)
+    else:
+        answer = Report().get_person_report(**arg)
 
     return jsonify(answer)
 
 
-@mod.route('/report/summ', methods=['POST'])
+@mod.route('/report/money', methods=['POST'])
 @login_required
 @json_headers
-def report_get_summ_report():
+def report_get_money_report():
     arg = get_post_arg(request, True)
 
     arg['payment_type'] = Report.TYPE_PAYMENT
