15ba13036fc14908751606b53c6e8cf74bfa3975
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-03-31 17:22:27 +0400

    fix: report stack model

--- models/report_stack.py
+++ models/report_stack.py
@@ -21,35 +21,48 @@ class ReportStack(db.Model, BaseModel):
     EXCEL_NO = 0
     EXCEL_YES = 1
 
+    INTERVAL_ONCE = 0
+    INTERVAL_DAY = 1
+    INTERVAL_WEEK = 2
+    INTERVAL_MONTH = 3
+
+    TYPE_PERSON = 0
+    TYPE_TERM = 1
+    TYPE_MONEY = 2
+
     id = db.Column(db.Integer, primary_key=True)
+    firm_id = db.Column(db.Integer, db.ForeignKey('firm.id'))
+    firm = db.relationship('Firm')
     email = db.Column(db.Text, nullable=False)
-    person = db.Column(db.Text, nullable=False)
-    firm_id = db.Column(db.Integer, nullable=False)
-    start = db.Column(db.Date, nullable=False)
-    stop = db.Column(db.Date, nullable=False)
     excel = db.Column(db.Integer, nullable=False)
-    type_id = db.Column(db.Integer, nullable=False)
-    check_summ = db.Column(db.String(32), nullable=False)
-    lock = db.Column(db.Integer, index=True, nullable=False)
+    type = db.Column(db.Integer, index=True, nullable=False)
+    interval = db.Column(db.Integer, nullable=False)
+    details = db.Column(db.Text)
+    launch_date = db.Column(db.DateTime)
+    lock = db.Column(db.Integer, nullable=False)
 
     def __init__(self):
+        self.excel = self.EXCEL_YES
+        self.type = TYPE_PERSON
+        self.interval = INTERVAL_ONCE
         self.lock = self.LOCK_FREE
-        self.excel = self.EXCEL_NO
 
-    def __repr__(self):
-        return '<id %r>' % (self.id)
+    def get_interval_list(self):
+        return [
+            {'id': self.INTERVAL_ONCE, 'name': u"Разовый"},
+            {'id': self.INTERVAL_DAY, 'name': u"Дневной"},
+            {'id': self.INTERVAL_WEEK, 'name': u"Недельный"},
+            {'id': self.INTERVAL_MONTH, 'name': u"Месячный"}
+        ]
+
+    def get_type_list(self):
+        return [
+            {'id': self.TYPE_PERSON, 'name': u"По людям"},
+            {'id': self.TYPE_TERM, 'name': u"По терминалам"},
+            {'id': self.TYPE_MONEY, 'name': u"Личные расходы"}
+        ]
 
     def get_json(self):
         self.email = json.loads(self.email)
         self.recipients = json.loads(self.recipients)
-
         return self
-
-    def get_check_summ(self):
-        return hashlib.md5("%s%s%s%s%s%s" % (
-            str(self.email),
-            str(self.person),
-            str(self.firm_id),
-            str(self.start),
-            str(self.stop),
-            str(self.type_id))).hexdigest()
