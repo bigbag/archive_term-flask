1964be1859298dadc4fa67aabc1c6e7ce42f598d
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-04-21 16:38:33 +0400

    refactor: request helper

--- helpers/request_helper.py
+++ helpers/request_helper.py
@@ -9,30 +9,21 @@ from grab import Grab
 import json
 
 
-def clear_get_params(str):
-
-    answer = str
-    if "/" in answer:
-        answer = answer[0:answer.find("/")]
-    if "?" in answer:
-        answer = answer[0:answer.find("?")]
-    if "&" in answer:
-        answer = answer[0:answer.find("&")]
+def clear_get_params(answer):
+    keys = ("/", "?", "&")
+    for key in keys:
+        if key in answer:
+            answer = answer[0:answer.find(key)]
+            break
 
     return answer
 
 
-def parse_get_param(url, param):
-    value = url
+def parse_get_param(value, param):
     if param in value:
         value = value[value.find(param) + len(param):]
 
-    if "/" in value:
-        value = value[0:value.find("/")]
-    if "?" in value:
-        value = value[0:value.find("?")]
-    if "&" in value:
-        value = value[0:value.find("&")]
+    value = clear_get_params(value)
 
     return value
 
