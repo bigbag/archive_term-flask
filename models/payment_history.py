ebdc4c48ddbc8adad8bff405966982fbd26ce0d5
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-06-06 18:31:33 +0400

    fix

--- models/payment_history.py
+++ models/payment_history.py
@@ -24,9 +24,9 @@ class PaymentHistory(db.Model, BaseModel):
     __tablename__ = 'history'
 
     STATUS_NEW = 0
-    STATUS_COMPLETE = 1
+    STATUS_IN_PROGRESS = 1
+    STATUS_COMPLETE = 2
     STATUS_FAILURE = -1
-    STATUS_MISSING = -2
 
     TYPE_SYSTEM = 1
     TYPE_PAYMENT = 2
@@ -76,8 +76,8 @@ class PaymentHistory(db.Model, BaseModel):
     def get_fail_linking_record(self, history_id, wallet_id):
         return PaymentHistory().query.filter(PaymentHistory.id != history_id).filter(
             PaymentHistory.wallet_id == wallet_id).filter(
-            PaymentHistory.type == PaymentHistory.TYPE_SYSTEM).filter(
-            PaymentHistory.status == PaymentHistory.STATUS_NEW).all()
+                PaymentHistory.type == PaymentHistory.TYPE_SYSTEM).filter(
+                    PaymentHistory.status == PaymentHistory.STATUS_NEW).all()
 
     def add_linking_record(self, user_id, wallet_id):
         self.type = PaymentHistory.TYPE_SYSTEM
@@ -102,6 +102,6 @@ class PaymentHistory(db.Model, BaseModel):
     def get_new_payment(self):
         return PaymentHistory.query.filter(
             PaymentHistory.report_id != 0).filter(
-            PaymentHistory.status == PaymentHistory.STATUS_NEW).filter(
-            PaymentHistory.request_id != 0).all(
-        )
+                PaymentHistory.status == PaymentHistory.STATUS_NEW).filter(
+                    PaymentHistory.request_id != 0).all(
+                    )
--- web/tasks/payment.py
+++ web/tasks/payment.py
@@ -52,12 +52,16 @@ class PaymentTask (object):
     def check_status(history_id):
         history = PaymentHistory.query.get(history_id)
         if not history:
-            app.logger.error('Check: Not found history, history_id=%s' % history_id)
+            app.logger.error(
+                'Check: Not found history, history_id=%s' %
+                history_id)
             return False
 
         wallet = PaymentWallet.query.get(history.wallet_id)
         if not wallet:
-            app.logger.error('Check: Not found wallet, wallet_id=%s' % wallet_id)
+            app.logger.error(
+                'Check: Not found wallet, wallet_id=%s' %
+                wallet_id)
             return False
 
         ym = YaMoneyApi(YandexMoneyConfig)
@@ -69,9 +73,15 @@ class PaymentTask (object):
             history.delete()
             return False
 
-        if result['status'] == 'success':
+        if result['status'] == 'in_progress':
+            history.status = PaymentHistory.STATUS_IN_PROGRESS
+            history.save()
+
+        elif result['status'] == 'success':
             if not 'invoice_id' in result:
-                app.logger.error('Check: Fail, not found invoice_id, history_id=%s' % history_id)
+                app.logger.error(
+                    'Check: Fail, not found invoice_id, history_id=%s' %
+                    history_id)
                 return False
 
             history.invoice_id = result['invoice_id']
@@ -94,12 +104,17 @@ class PaymentTask (object):
         status = False
         report = Report.query.get(report_id)
         if not report:
-            app.logger.error('Payment: Not found report with id %s' % report.id)
+            app.logger.error(
+                'Payment: Not found report with id %s' %
+                report.id)
             return False
 
-        wallet = PaymentWallet.query.filter_by(payment_id=report.payment_id).first()
+        wallet = PaymentWallet.query.filter_by(
+            payment_id=report.payment_id).first()
         if not wallet:
-            app.logger.error('Payment: Not found wallet with pid %s' % report.payment_id)
+            app.logger.error(
+                'Payment: Not found wallet with pid %s' %
+                report.payment_id)
             return False
 
         history = PaymentHistory.query.filter_by(report_id=report.id).first()
@@ -108,13 +123,15 @@ class PaymentTask (object):
 
         history = PaymentHistory().from_report(report, wallet)
         if not history:
-            app.logger.error('Payment: Fail in history add, report_id=%s' % report.id)
+            app.logger.error(
+                'Payment: Fail in history add, report_id=%s' %
+                report.id)
             return False
 
         card = PaymentCard.query.filter_by(
             wallet_id=wallet.id,
             status=PaymentCard.STATUS_PAYMENT).first(
-        )
+            )
         if not card:
             history.delete()
             wallet.add_to_blacklist()
@@ -126,20 +143,26 @@ class PaymentTask (object):
 
         amount = int(
             report.amount) / int(
-            Term.DEFAULT_FACTOR) * int(
-            term.factor)
+                Term.DEFAULT_FACTOR) * int(
+                    term.factor)
 
         ym = YaMoneyApi(YandexMoneyConfig)
-        payment = ym.get_request_payment_to_shop(amount, ym.const.PAYMENT_PATTERN_ID)
+        payment = ym.get_request_payment_to_shop(
+            amount, ym.const.PAYMENT_PATTERN_ID)
         if not payment or not 'request_id' in payment:
-            app.logger.error('Payment: Fail in request payment, report_id %s' % report_id)
+            app.logger.error(
+                'Payment: Fail in request payment, report_id %s' %
+                report_id)
             wallet.add_to_blacklist()
             history.delete()
             return False
 
-        result = ym.get_process_external_payment(payment['request_id'], card.token)
+        result = ym.get_process_external_payment(
+            payment['request_id'], card.token)
         if result['status'] not in ('success', 'in_progress'):
-            app.logger.error('Payment: Fail in process payment, report_id %s' % report_id)
+            app.logger.error(
+                'Payment: Fail in process payment, report_id %s' %
+                report_id)
             wallet.add_to_blacklist()
             history.delete()
             return False
