f03ac3c9ce895d9de9555a04160f3d4af1c914bd
Denis Amelin <dionicii@mail.ru>
2014-12-01 19:30:09 +0400

    Миграции для Счета

---
+++ migrations/versions/4c29f0d0b393_add_account_email_column_in_firm.py
@@ -0,0 +1,8 @@
+"""add account_email column in firm
+Revision ID: 4c29f0d0b393Revises: 132424e9f86bCreate Date: 2014-11-20 17:53:00.628000
+"""
+# revision identifiers, used by Alembic.
+revision = '4c29f0d0b393'down_revision = '132424e9f86b'
+from alembic import op
+import sqlalchemy as sa
+from alembic import opimport sqlalchemy as sadef upgrade(engine_name):    eval("upgrade_%s" % engine_name)()def downgrade(engine_name):    eval("downgrade_%s" % engine_name)()def upgrade_term():    op.add_column('firm', sa.Column(        'account_email', sa.Text(), nullable=True))def downgrade_term():    op.drop_column('firm', 'account_email')def upgrade_stack():    passdef downgrade_stack():    passdef upgrade_payment():    pass    def downgrade_payment():    passdef upgrade_mobispot():    passdef downgrade_mobispot():    pass
---
+++ migrations/versions/51698e9f4ac9_add_legal_entity_column_in_firm.py
@@ -0,0 +1,6 @@
+"""add legal_entity column in firm
+Revision ID: 51698e9f4ac9Revises: 4c29f0d0b393Create Date: 2014-11-27 10:44:59.733000
+"""
+# revision identifiers, used by Alembic.
+revision = '51698e9f4ac9'down_revision = '4c29f0d0b393'
+from alembic import opimport sqlalchemy as sadef upgrade(engine_name):    eval("upgrade_%s" % engine_name)()def downgrade(engine_name):    eval("downgrade_%s" % engine_name)()def upgrade_term():    op.add_column('firm', sa.Column(        'legal_entity', sa.String(256), nullable=True))def downgrade_term():    op.drop_column('firm', 'legal_entity')def upgrade_stack():    passdef downgrade_stack():    passdef upgrade_payment():    pass    def downgrade_payment():    passdef upgrade_mobispot():    passdef downgrade_mobispot():    pass
