e3964cfe089a432006d8180e109118ab563f90c4
Denis Amelin <dionicii@mail.ru>
2015-01-22 10:15:35 +0300

    feature: сумма задолженности в письме-уведомлении о попадани спота в черный список. Новые шаблоны писем.

---
+++ migrations/versions/5ae21ae78a54_add_payment_id_column_in_payment_fail.py
@@ -0,0 +1,4 @@
+"""add_payment_id_column_in_payment_fail
+Revision ID: 5ae21ae78a54Revises: 4629ec9a2764Create Date: 2015-01-20 15:14:48.466000
+"""
+# revision identifiers, used by Alembic.revision = '5ae21ae78a54'down_revision = '4629ec9a2764'from alembic import opimport sqlalchemy as sadef upgrade(engine_name):    eval("upgrade_%s" % engine_name)()def downgrade(engine_name):    eval("downgrade_%s" % engine_name)()def upgrade_term():    passdef downgrade_term():    passdef upgrade_stack():    passdef downgrade_stack():    passdef upgrade_payment():    op.add_column('fail', sa.Column(        'payment_id', sa.String(20), nullable=True))def downgrade_payment():    op.drop_column('fail', 'payment_id')def upgrade_mobispot():    passdef downgrade_mobispot():    pass
